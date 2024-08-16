d3558bb904fdb8e54c082acd9e9edae23e6e2590
KadenSeaman <kadencs24@outlook.com>
2024-08-15 21:22:39 -0600

    look at this graph

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,28 @@ import * as d3 from "d3";
 function BarChart({dataset}){
 
   const barChartRef = useRef(null);
+  const [barChartExists, setBarChartExists] = useState('false');
 
   useEffect(() => {
 
     if (!dataset) return;
 
+    if(barChartExists){
+      d3.select(barChartRef.current).selectAll("*").remove()
+    }
+
+    setBarChartExists(true);
+
     const width = 800;
     const height = 400;
 
-    const marginTop = 30;
+    const marginTop = 50;
     const marginRight = 20;
-    const marginBottom = 30;
+    const marginBottom = 50;
     const marginLeft = 40;
 
+
+    // x and y axis scale information
     const startDate = new Date(d3.min(dataset, d => d[0]));
     const endDate = new Date(d3.max(dataset, d => d[0]));
 
@@ -48,7 +57,21 @@ function BarChart({dataset}){
     .attr('transform',`translate(${marginLeft},0)`)
     .attr('id','y-axis')
     .call(yAxis);
-    
+
+    //yAxis label
+    svg.append('text')
+    .attr('x', -275)
+    .attr('y', marginLeft * 1.5)
+    .attr('transform','rotate(-90)')
+    .text('United States GDP (in billions)')
+
+    //xAxis more info label
+    svg.append('text')
+    .attr('x', width - 435)
+    .attr('y', height - 5)
+    .text("More Information: http://www.bea.gov/national/pdf/nipaguid.pdf")
+
+    //bar sizing scale information
     const heightLinearScale = d3.scaleLinear()
     .domain([0,d3.max(dataset, d => d[1])])
     .range([marginBottom, height - marginTop]);
@@ -57,6 +80,9 @@ function BarChart({dataset}){
     .domain([startDate,endDate])
     .range([marginLeft, width - marginRight]);
 
+    
+
+    //individual bars
     svg.selectAll("rect")
     .data(dataset)
     .enter()
@@ -67,7 +93,56 @@ function BarChart({dataset}){
     .attr('height', d => heightLinearScale(d[1]) - marginBottom)
     .attr('width', (width - marginLeft - marginRight) / 275 + 0.03)
     .attr('x', d => xLinearScale(new Date(d[0]))) 
-    .attr('y', d => height - heightLinearScale(d[1]));
+    .attr('y', d => height - heightLinearScale(d[1]))
+    .on('mouseover', (event, d) => {
+      // tooltip.selectAll("*").remove();
+
+      const year = d[0].slice(0,4)
+      let quarter = d[0].slice(5,7);
+      let amount = d[1];
+
+      switch(quarter){
+        case "01":
+          quarter = "Q1";
+          break;
+        case "04":
+          quarter = "Q2";
+          break;
+        case "07":
+          quarter = "Q3";
+          break;
+        case "10":
+          quarter = "Q4";
+          break;
+        default:
+          quarter = "ERROR"
+          break;
+      }
+
+
+      const xPosition = xLinearScale(new Date(d[0]));
+      if(xPosition < width / 2){
+        tooltip.style('left',  xPosition + 10 + "px");
+      }
+      else{
+        tooltip.style('left',xPosition - 150 + "px")
+      }
+      tooltip.style('bottom', height / 3 + "px")
+      
+      tooltip.attr('data-date',d[0]);
+      tooltip.html(year + " " + quarter + "<br>" + "$" + amount + " billion");
+      tooltip.style('opacity',1);
+    })
+    .on('mouseout', () => {
+      tooltip.style('opacity', 0)
+    })
+
+    //overlay
+    const tooltip = d3.select(".main-card")
+    .append('div')
+    .attr('x',200)
+    .attr('y', 300)
+    .attr('id','tooltip')
 
   },[dataset])
 
@@ -84,10 +159,13 @@ function App() {
   },[])
 
   return (
-    <div className="main-card">
+    <div className="wrapper">
+      <div className="main-card">
       <h1 id="title">United States GDP</h1>
       <BarChart dataset = {dataSet} />
+      </div>
     </div>
+
   )
 }
 
